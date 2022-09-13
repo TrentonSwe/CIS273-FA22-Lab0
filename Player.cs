3ec39c4f93ee4eddede3382b6a7f192503ba9cba
TrentonSwe <89215748+TrentonSwe@users.noreply.github.com>
2022-09-12 23:50:33 -0500

    Add files via upload

---
+++ Player.cs
@@ -0,0 +1,34 @@
+﻿using System;
+namespace Uno
+{
+	public class Player
+	{
+		public string Name { get; set; }
+        public List<Card> Hand { get; set; }
+
+
+        public Player()
+		{
+			Name = "";
+			Hand = new List<Card>();
+		}
+
+        public bool HasPlayableCard(Card card)
+		{
+			return false;
+		}
+
+        public Card GetFirstPlayableCard(Card card)
+		{
+			return new Card();
+		}
+
+        public Color MostCommonColor()
+		{
+			return Color.Blue;
+		}
+
+
+    }
+}
+
